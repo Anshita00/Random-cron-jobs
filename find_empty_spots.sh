156f26669ac1ee95011801c59d217cfa5a894fbe
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-11-18 19:24:29 +1100

    Run script in bash

--- find_empty_spots.sh
+++ find_empty_spots.sh
@@ -1,26 +1,3 @@
-#!/bin/sh
-
-curl 'http://timetable.unsw.edu.au/2019/COMP3231.html' > tmp.html
-nums=`egrep -o '[0-9]{3}/[0-9]{3}' tmp.html | cut -d '/' -f1 | uniq`
-echo "$nums"
-out_of=`egrep -o '[0-9]{3}/[0-9]{3}' tmp.html | cut -d '/' -f2 |  uniq`
-echo "$out_of"
-if test $nums -lt $out_of
-then
-	echo "GO AND ENROL IN OS NOW!" > body
-	mailx -s "URGENT! SPOT AVAILABLE IN OS!" "anshitaggarwal2000@gmail.com" < body
-fi
-
-curl 'http://timetable.unsw.edu.au/2019/COMP6441.html' > tmp2.html
-nums=`egrep -o '[0-9]{3}/[0-9]{3}' tmp2.html | cut -d '/' -f1 | uniq`
-echo "$nums"
-out_of=`egrep -o '[0-9]{3}/[0-9]{3}' tmp2.html | cut -d '/' -f2 |  uniq`
-echo "$out_of"
-if test $nums -lt $out_of
-then
-	echo "GO AND ENROL IN SECURITY NOW!" > body
-	mailx -s "URGENT! SPOT AVAILABLE IN SECURITY!" "anshitaggarwal2000@gmail.com" < body
-fi
-
+#!/usr/bin/env bash
 
 
